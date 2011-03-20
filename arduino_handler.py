75986e69bde79c4f4aa5391542f8ecca6ea14bb5
Marcel Caraciolo <caraciol@gmail.com>
2011-03-20 16:00:28 -0300

    Modificado o Arduino Handler.

--- arduino_handler.py
+++ arduino_handler.py
@@ -19,42 +19,56 @@ import sys
 PASS = 1
 FAIL = 0
 
-def send_message(test_code):
-    '''
-    Send a message to Arduino via serial
-    '''
-    if sys.platform == 'linux2':
-        for i in range(0,45):
-            try:
-                 arduino = serial.Serial('/dev/ttyACM%d' %i, 9600)
-                 break
-            except SerialException:
-                 pass
-    elif sys.platform == 'darwin':
-        for i in range(0,500):
-            try:
-                 arduino = serial.Serial('/dev/cu.usbmodem%d' %i, 9600)
-                 break
-            except SerialException:
-                 pass  
-    else:
-        for i in range(0,45):
-            try:
-                 arduino = serial.Serial('COM%d' % i, 9600)
-                 break
-            except SerialException:
-                 pass  
-   
-    if test_code == PASS:
-        arduino.write('1')
-    else
-        arduino.write('0')
+GREEN = '1'
+RED = '0'
+
+class Arduino(object):
+    
+    def __init__(self):
+        self.arduino = None
+        
+        if sys.platform == 'linux2':
+            for i in range(0,45):
+                try:
+                     self.arduino = serial.Serial('/dev/ttyACM%d' %i, 9600)
+                     break
+                except serial.serialutil.SerialException:
+                     pass
+        elif sys.platform == 'darwin':
+            for i in range(0,500):
+                try:
+                     self.arduino = serial.Serial('/dev/cu.usbmodem%d' %i, 9600)
+                     break
+                except serial.serialutil.SerialException:
+                     pass  
+        else:
+            for i in range(0,45):
+                try:
+                     self.arduino = serial.Serial('COM%d' % i, 9600)
+                     break
+                except serial.SerialException:
+                     pass
+        if not self.arduino:
+            print 'ERRO: Arduino nao encontrado. Favor verificar a porta correta.'
+            exit()
+            
+
+    def send_message(self,test_code):
+        '''
+        Send a message to Arduino via serial
+        '''   
+        if test_code == PASS:
+            self.arduino.write(GREEN)
+        else:
+            self.arduino.write(RED)
 
      
         
 
 
-
+if __name__ ==  '__main__':
+    handler = Arduino()
+    handler.send_message(PASS)
 
 
 
--- teste.py
+++
@@ -1,9 +0,0 @@
-import serial,pygame,time
-
-ser = serial.Serial('/dev/cu.usbmodem411',9600)
-
-
-
-
-		
-
